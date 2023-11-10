0ce69ebafea4f57577e583ebf4de01e13bd1b0cd
Simon Keefe <simon@skeefe.net>
2023-11-10 20:58:11 +1100

    hhhhhhhhhhhh

--- firebase/getCollectionSnapshot.ts
+++ firebase/getCollectionSnapshot.ts
@@ -1,26 +1,17 @@
 import { useState, useEffect } from "react";
 import firebase_app from "./config";
-<<<<<<< HEAD
-import { getFirestore, collection, onSnapshot, query, orderBy } from "firebase/firestore";
-=======
 import {
   getFirestore,
   collection,
   onSnapshot,
   query,
 } from "firebase/firestore";
->>>>>>> 02af7ab398ef14e64b9e2910b00289926e7281f0
 
 export default function getCollectionShapshot(fbCollection) {
   const db = getFirestore(firebase_app);
 
   const [fbData, setFBData] = useState([]);
 
-<<<<<<< HEAD
-    useEffect(() => {
-        const collectionRef = collection(db, colllection);
-        const q = query(collectionRef, orderBy("Date", "desc"));
-=======
   useEffect(() => {
     const collectionRef = collection(db, fbCollection);
     const q = query(collectionRef); //, orderBy('createdAt','desc'));
@@ -35,7 +26,6 @@ export default function getCollectionShapshot(fbCollection) {
         })
       );
     });
->>>>>>> 02af7ab398ef14e64b9e2910b00289926e7281f0
 
     return unsubscribe;
   }, []);
