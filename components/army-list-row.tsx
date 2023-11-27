bda6344be2750ada0403114df9999736b2ed4c37
Simon Keefe <simon@skeefe.net>
2023-11-27 17:14:15 +1100

    Installed Prettier.

--- components/army-list-row.tsx
+++ components/army-list-row.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 import { useRouter } from "next/router";
 
 export default function ArmyListRow(army) {
-
   const router = useRouter();
 
-  const primaryPointsFor = isNaN(army.PrimaryPointsFor) ? 0 : army.PrimaryPointsFor;
-  const primaryPointsAgainst = isNaN(army.PrimaryPointsAgainst) ? 0 : army.PrimaryPointsAgainst;
-  const secondaryPointsFor = isNaN(army.SecondaryPointsFor) ? 0 : army.SecondaryPointsFor;
-  const secondaryPointsAgainst = isNaN(army.SecondaryPointsAgainst) ? 0 : army.SecondaryPointsAgainst;
+  const primaryPointsFor = isNaN(army.PrimaryPointsFor)
+    ? 0
+    : army.PrimaryPointsFor;
+  const primaryPointsAgainst = isNaN(army.PrimaryPointsAgainst)
+    ? 0
+    : army.PrimaryPointsAgainst;
+  const secondaryPointsFor = isNaN(army.SecondaryPointsFor)
+    ? 0
+    : army.SecondaryPointsFor;
+  const secondaryPointsAgainst = isNaN(army.SecondaryPointsAgainst)
+    ? 0
+    : army.SecondaryPointsAgainst;
   //const lastFiveRecord = army.Record.slice(Math.max(army.Record.length - 5, 0))
 
   function handleRowClick(id) {
@@ -17,25 +24,35 @@ export default function ArmyListRow(army) {
 
   return (
     <>
-      <tr
-        onClick={() => handleRowClick(army.id)}
-        className="cursor-pointer"
-      >
-        <td>{army.Name}</td>  
-        <td className="text-center">{army.Played}</td>       
-        <td className="text-center">{army.Won}</td>        
+      <tr onClick={() => handleRowClick(army.id)} className="cursor-pointer">
+        <td>{army.Name}</td>
+        <td className="text-center">{army.Played}</td>
+        <td className="text-center">{army.Won}</td>
         <td className="text-center">{army.Lost}</td>
-        <td className="text-center">{Math.round((primaryPointsFor + secondaryPointsFor)/army.Played*10)/10}</td>
+        <td className="text-center">
+          {Math.round(
+            ((primaryPointsFor + secondaryPointsFor) / army.Played) * 10
+          ) / 10}
+        </td>
         <td className="text-center">{primaryPointsFor + secondaryPointsFor}</td>
-        <td className="text-center">{primaryPointsFor + secondaryPointsFor - primaryPointsAgainst - secondaryPointsAgainst}</td>        
-        <td className="text-center">{Math.round((army.Won/army.Played)*1000)/10 + "%"}</td>
+        <td className="text-center">
+          {primaryPointsFor +
+            secondaryPointsFor -
+            primaryPointsAgainst -
+            secondaryPointsAgainst}
+        </td>
+        <td className="text-center">
+          {Math.round((army.Won / army.Played) * 1000) / 10 + "%"}
+        </td>
         {/*
         <td className="text-center">
           {lastFiveRecord.map((record) => record.Result)}
         </td>
         */}
-        <td className="text-center">{Math.round((army.FirstTurn/army.Played)*1000)/10 + "%"}</td>
+        <td className="text-center">
+          {Math.round((army.FirstTurn / army.Played) * 1000) / 10 + "%"}
+        </td>
       </tr>
     </>
   );
-};
+}
--- components/army-list.tsx
+++ components/army-list.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import getCollectionSnapshot from "../firebase/getCollectionSnapshot";
 import ArmyListRow from "./army-list-row";
 
-
 const ArmyList = () => {
   const armyCollection = getCollectionSnapshot("Armies", "Name", "asc");
-  const activeArmyCollection = armyCollection.filter(obj => Object.keys(obj).includes("Played"));
+  const activeArmyCollection = armyCollection.filter((obj) =>
+    Object.keys(obj).includes("Played")
+  );
 
   return (
     <>
       <div className="lg:flex gap-x-12">
         <section id="armyList" className="lg:flex-1">
-
           <h1 className="text-2xl md:text-4xl font-bold text-center mb-4 md:mb-8">
             Armies
           </h1>
@@ -20,15 +20,15 @@ const ArmyList = () => {
             <thead>
               <tr>
                 <th>Name</th>
-                <th className='text-center'>Played</th>
-                <th className='text-center'>Won</th>
-                <th className='text-center'>Lost</th>
-                <th className='text-center'>Avg. Points</th>
-                <th className='text-center'>Total Points</th>
-                <th className='text-center'>Points +/-</th>
-                <th className='text-center'>Win %</th>
+                <th className="text-center">Played</th>
+                <th className="text-center">Won</th>
+                <th className="text-center">Lost</th>
+                <th className="text-center">Avg. Points</th>
+                <th className="text-center">Total Points</th>
+                <th className="text-center">Points +/-</th>
+                <th className="text-center">Win %</th>
                 {/* <th className='text-center'>Last 5</th> */}
-                <th className='text-center'>First Turn %</th>
+                <th className="text-center">First Turn %</th>
               </tr>
             </thead>
             <tbody>
@@ -37,7 +37,6 @@ const ArmyList = () => {
               ))}
             </tbody>
           </table>
-
         </section>
       </div>
     </>
