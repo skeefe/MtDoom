e2f99210caa3a467104d9c1f542693577dc459af
Simon Keefe <simon@skeefe.net>
2023-12-09 01:34:54 +1100

    Bug fixes to deploy.

--- firebase/addData.ts
+++ firebase/addData.ts
@@ -3,8 +3,8 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const db = getFirestore(firebase_app);
 export default async function addData(colllection, id, data) {
-  let result = null;
-  let error = null;
+  let result; // = null;
+  let error; // = null;
 
   try {
     result = await setDoc(doc(db, colllection, id), data, {
