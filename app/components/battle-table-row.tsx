563a6b754abcb2155efad6e70c25dbacf126b9fa
Simon Keefe <simon@skeefe.net>
2023-12-10 21:59:33 +1100

    Emoji and nickname support - battle list.

--- app/components/battle-table-row.tsx
+++ app/components/battle-table-row.tsx
@@ -4,9 +4,33 @@ import { formatDate } from "../../utils/date-format";
 import getArmyName from "../firebase/getArmyName";
 import rowClick from "../../utils/row-click";
 import { useRouter } from "next/navigation";
+import getDocSnapshot from "../firebase/getDocSnapshot";
 
 const BattleTableRow = (props: { battle: battleSummary }) => {
   const router = useRouter();
+
+  const getArmyTitle = (armyId: string) => {
+    const armyDoc = getDocSnapshot("Armies", armyId);
+
+    if (armyDoc["Emoji"] !== undefined) {
+      return `${armyDoc["Emoji"]} ${armyDoc["Name"]}`;
+    } else {
+      return armyDoc["Name"];
+    }
+  };
+
+  const getGeneralNickname = (generalId: string) => {
+    const armyDoc = getDocSnapshot("Generals", generalId);
+    if (armyDoc["Nicknames"] !== undefined && armyDoc["Nicknames"].length > 0) {
+      const randomNickname =
+        armyDoc["Nicknames"][
+          Math.floor(Math.random() * armyDoc["Nicknames"].length)
+        ];
+      return randomNickname;
+    } else {
+      return armyDoc["Alias"];
+    }
+  };
 
   return (
     <>
@@ -22,18 +46,18 @@ const BattleTableRow = (props: { battle: battleSummary }) => {
         </td>
         <td>
           <span className="cell-heading">
-            {getArmyName(props.battle.AttackerArmy)}
+            {getArmyTitle(props.battle.AttackerArmy)}
             {props.battle.IsAttackerVictor ? " 🎖" : null}
           </span>
-          <span>General: {props.battle.Attacker}</span>
+          <span>General: {getGeneralNickname(props.battle.Attacker)}</span>
           <span>Total: {props.battle.TotalAttacker}</span>
         </td>
         <td>
           <span className="cell-heading">
-            {getArmyName(props.battle.DefenderArmy)}
+            {getArmyTitle(props.battle.DefenderArmy)}
             {props.battle.IsDefenderVictor ? " 🎖" : null}
           </span>
-          <span>General: {props.battle.Defender}</span>
+          <span>General: {getGeneralNickname(props.battle.Defender)}</span>
           <span>Total: {props.battle.TotalDefender}</span>
         </td>
       </tr>
---
+++ app/firebase/getDocSnapshot.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import firebase_app from "./config";
+import { getFirestore, onSnapshot, doc } from "firebase/firestore";
+
+export default function getDocSnapshot(collectionId: string, docId: string) {
+  const [fbData, setFBData] = useState({});
+
+  const db = getFirestore(firebase_app);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, collectionId, docId), (doc) => {
+      setFBData(() => {
+        return {
+          ...doc.data(),
+        };
+      });
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return fbData;
+}
