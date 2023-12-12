fa918a29cc807661b84e712e263aaf2e9e86b650
Simon Keefe <simon@skeefe.net>
2023-12-12 21:17:12 +1100

    General Dashboard continued.

--- app/components/general-dashboard.tsx
+++ app/components/general-dashboard.tsx
@@ -15,19 +15,68 @@ import { iBattle } from "../types/battle";
 import getDocSnapshot from "../firebase/getDocSnapshot";
 
 const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
-  let opponentBattles: {
+  let opponentGeneralBattles: {
+    General: string;
+    Played: number;
+    Won: number;
+    Lost: number;
+  }[] = [];
+
+  const getOpponentGeneralRecordData = () => {
+    props.battles.forEach(groupOpponentGeneralBattles);
+    return opponentGeneralBattles;
+  };
+
+  const groupOpponentGeneralBattles = (battle) => {
+    //Get opponent General.
+    const opponentGeneralId =
+      battle.Attacker === props.general.id ? battle.Defender : battle.Attacker;
+
+    const opponentGeneralName = getGeneralAlias(opponentGeneralId);
+
+    const victor =
+      battle.Attacker === battle.Victor ? battle.Attacker : battle.Defender;
+
+    //Check if it exists in the opponentBattles array, if not add it (empty).
+    if (
+      !opponentGeneralBattles.some(
+        (opponent) => opponent["General"] === opponentGeneralName
+      )
+    ) {
+      opponentGeneralBattles.push({
+        General: opponentGeneralName,
+        Played: 0,
+        Won: 0,
+        Lost: 0,
+      });
+    }
+
+    //Find the opponent armies object.
+    let opponent = opponentGeneralBattles.find(
+      (army) => army["General"] === opponentGeneralName
+    );
+
+    //Increment
+    opponent && ++opponent["Played"];
+    opponent && victor === props.general.id && ++opponent["Won"];
+    opponent && victor !== props.general.id && ++opponent["Lost"];
+  };
+
+  //----
+
+  let opponentArmyBattles: {
     Army: string;
     Played: number;
     Won: number;
     Lost: number;
   }[] = [];
 
-  const getOpponentRecordData = () => {
-    props.battles.forEach(groupOpponentBattles);
-    return opponentBattles;
+  const getOpponentArmyRecordData = () => {
+    props.battles.forEach(groupOpponentArmyBattles);
+    return opponentArmyBattles;
   };
 
-  const groupOpponentBattles = (battle) => {
+  const groupOpponentArmyBattles = (battle) => {
     //Get opponent Army.
     const opponentArmyId =
       battle.Attacker === props.general.id
@@ -41,9 +90,11 @@ const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
 
     //Check if it exists in the opponentBattles array, if not add it (empty).
     if (
-      !opponentBattles.some((opponent) => opponent["Army"] === opponentArmyName)
+      !opponentArmyBattles.some(
+        (opponent) => opponent["Army"] === opponentArmyName
+      )
     ) {
-      opponentBattles.push({
+      opponentArmyBattles.push({
         Army: opponentArmyName,
         Played: 0,
         Won: 0,
@@ -52,7 +103,7 @@ const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
     }
 
     //Find the opponent armies object.
-    let opponent = opponentBattles.find(
+    let opponent = opponentArmyBattles.find(
       (army) => army["Army"] === opponentArmyName
     );
 
@@ -62,19 +113,35 @@ const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
     opponent && victor !== props.general.id && ++opponent["Lost"];
   };
 
+  //Utils
+  const getGeneralAlias = (generalId: string) => {
+    const generalDoc = getDocSnapshot("Generals", generalId);
+    return generalDoc["Alias"];
+  };
+
   const getArmyName = (armyId: string) => {
     const armyDoc = getDocSnapshot("Armies", armyId);
     return armyDoc["Name"];
   };
 
-  //Get the max number of games played.
-  let maxPlayed = 0;
-  const yAxisLength = () => {
-    opponentBattles.map((battle) => {
+  //Get the max number of General games played.
+  let maxGeneralPlayed = 0;
+  const yAxisGeneralLength = () => {
+    opponentGeneralBattles.map((battle) => {
       const played = battle.Played;
-      maxPlayed = Math.max(maxPlayed, played);
+      maxGeneralPlayed = Math.max(maxGeneralPlayed, played);
     });
-    return maxPlayed;
+    return maxGeneralPlayed;
+  };
+
+  //Get the max number of Army games played.
+  let maxArmyPlayed = 0;
+  const yAxisArmyLength = () => {
+    opponentArmyBattles.map((battle) => {
+      const played = battle.Played;
+      maxArmyPlayed = Math.max(maxArmyPlayed, played);
+    });
+    return maxArmyPlayed;
   };
 
   return props.battles ? (
@@ -85,7 +152,7 @@ const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
             <h3>Record vs Army</h3>
             <ResponsiveContainer width="100%" height="100%">
               <BarChart
-                data={getOpponentRecordData()}
+                data={getOpponentArmyRecordData()}
                 barCategoryGap={5}
                 margin={{ left: -43, bottom: 58 }}
                 width={730}
@@ -93,7 +160,38 @@ const GeneralDashboard = (props: { general: iGeneral; battles: iBattle[] }) => {
               >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="Army" />
-                <YAxis allowDecimals={false} domain={[0, yAxisLength()]} />
+                <YAxis allowDecimals={false} domain={[0, yAxisArmyLength()]} />
+                <Tooltip
+                  wrapperStyle={{
+                    color: "#64748b",
+                    backgroundColor: "#fff",
+                    border: "2px solid #1e293b",
+                  }}
+                  cursor={false}
+                />
+                <Legend verticalAlign="bottom" />
+
+                <Bar dataKey="Won" stackId="a" fill="#667b99" label="1234" />
+                <Bar dataKey="Lost" stackId="a" fill="#94a3b8" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+          <div className="dashboard-panel">
+            <h3>Record vs General</h3>
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                data={getOpponentGeneralRecordData()}
+                barCategoryGap={5}
+                margin={{ left: -43, bottom: 58 }}
+                width={730}
+                height={250}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="General" />
+                <YAxis
+                  allowDecimals={false}
+                  domain={[0, yAxisGeneralLength()]}
+                />
                 <Tooltip
                   wrapperStyle={{
                     color: "#64748b",
