eb70ef50613588869001e950b18e1e5ce3f56a7e
Simon Keefe <simon@skeefe.net>
2025-06-13 17:52:11 +1000

    Challenger and Twist cards data added

---
+++ data/challenger-cards.ts
@@ -0,0 +1,50 @@
+import { selectOption } from "../app/types/select-option";
+
+const challengerCards: selectOption[] = [
+  {
+    Label: "Attrition => Pivotal Movement",
+    Value: "Attrition => Pivotal Movement",
+    Active: true,
+  },
+  {
+    Label: "Dug In => Harboured Power",
+    Value: "Dug In => Harboured Power",
+    Active: true,
+  },
+  {
+    Label: "Establish Comms => Renewed Focus",
+    Value: "Establish Comms => Renewed Focus",
+    Active: true,
+  },
+  {
+    Label: "Focused Effort => Burst of Speed",
+    Value: "Focused Effort => Burst of Speed",
+    Active: true,
+  },
+  {
+    Label: "Over the Line => Great Haste",
+    Value: "Over the Line => Great Haste",
+    Active: true,
+  },
+  {
+    Label: "Secure Extraction Zone => Force a Breach",
+    Value: "Secure Extraction Zone => Force a Breach",
+    Active: true,
+  },
+  {
+    Label: "Self Preservation => All In",
+    Value: "Self Preservation => All In",
+    Active: true,
+  },
+  {
+    Label: "Sow Chaos => Opportunistic Strike",
+    Value: "Sow Chaos => Opportunistic Strike",
+    Active: true,
+  },
+  {
+    Label: "Zone Defence => Strategic Retreat",
+    Value: "Zone Defence => Strategic Retreat",
+    Active: true,
+  },
+];
+export { challengerCards };
---
+++ data/twist-cards.ts
@@ -0,0 +1,14 @@
+import { selectOption } from "../app/types/select-option";
+
+const twistCards: selectOption[] = [
+  { Label: "Adapt or Die", Value: "Adapt or Die", Active: true },
+  { Label: "Bloodlust", Value: "Bloodlust", Active: true },
+  { Label: "High Octane", Value: "High Octane", Active: true },
+  { Label: "Lords of War", Value: "Lords of War", Active: true },
+  { Label: "Martial Pride", Value: "Martial Pride", Active: true },
+  { Label: "Night Fighting", Value: "Night Fighting", Active: true },
+  { Label: "Point Blank", Value: "Point Blank", Active: true },
+  { Label: "Rapid Escalation", Value: "Rapid Escalation", Active: true },
+  { Label: "Ruinscape", Value: "Ruinscape", Active: true },
+];
+export { twistCards };
