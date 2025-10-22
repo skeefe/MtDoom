67d0174092660885afe1ec392d41ea909240019b
skeefe <simon@skeefe.net>
2025-10-22 20:32:32 +1100

    Fix battle page.

--- app/battle/[battleId]/page.tsx
+++ app/battle/[battleId]/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { use } from 'react';
 import BattleForm from "../../components/battle-form";
 
-export default function Battle({ params }: { params: { battleId: string } }) {
-  const battleId = params.battleId;
+export default function Battle({ params }: { params: Promise<{ battleId: string }> }) {
+  const battleId = use(params).battleId;
 
   return (
     <>
